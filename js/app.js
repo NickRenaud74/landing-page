d58f2c2654f77202d417db9f1094753d69d914d7
Nick Renaud <nick_renaud@hotmail.com>
2020-08-14 11:50:19 -0700

    fixed nav bar and home button

--- js/app.js
+++ js/app.js
@@ -1,51 +1,74 @@
+
 // Global Variables
 const sections = document.querySelectorAll('section');
 const nav = document.querySelector('#navbar');
-const fragment = document.createDocumentFragment();
-
-
+const start = performance.now();
 
 // Create dynamic NavBar
-for (let section of sections) {
-    const navMenu = document.createElement('li');
-    const menuItem = section.getAttribute('data-nav');
-    navMenu.innerHTML = `<a href="#${section.id}" class="nav-link">${menuItem}</a>`;
-    fragment.appendChild(navMenu);
+const createNavBar = () => {
+    const fragment = document.createDocumentFragment();
+    for (let section of sections) {
+        const navMenu = document.createElement('li');
+        const menuItem = section.dataset.nav;
+        navMenu.innerHTML = `<a href="#${section.id}" class="nav-link ${section.id}">${menuItem}</a>`;
+        fragment.appendChild(navMenu);
+    }
+    nav.appendChild(fragment);
 }
-nav.appendChild(fragment);
-
-
-
+createNavBar(sections);
 
 // Make sections ACTIVE
-const getSectionLocation = sections => {
+const makeActive = () => {
     for (let section of sections) {
         const location = section.getBoundingClientRect();
-        if (location.top >= -10 && location.bottom <= window.innerHeight) {
+        const navRect = document.querySelector('#navbar').getBoundingClientRect();
+        if (location.top >= navRect.bottom && location.bottom <= window.innerHeight) {
             section.classList.add('active');
-            console.log(section.classList);
-            break;
         }
         else {
             section.classList.remove('active');
-            console.log(section.classList);
         }
     }
 }
 
-window.addEventListener('scroll', getSectionLocation(sections));
+
+window.addEventListener('scroll', () => {
+    makeActive()
+});
 
 //Scroll to anchors
-const links = document.querySelectorAll('.nav-link');
-
-for (let link of links) {
-    link.addEventListener('click', function (event) {
-        event.preventDefault();
-        const sectionId = link.getAttribute('href').slice(1);
-        rect = document.getElementById(`${sectionId}`).getBoundingClientRect();
-        window.scrollTo({
-            top: rect.top,
-            behavior: 'smooth'
+const smoothScroll = () => {
+    const links = document.querySelectorAll('.nav-link');
+    for (let link of links) {
+        link.addEventListener('click', (event) => {
+            event.preventDefault();
+            const sectionId = link.getAttribute('href').slice(1);
+            rect = document.getElementById(`${sectionId}`);
+            rect.scrollIntoView({
+                behavior: 'smooth',
+                block: 'center'
+            });
         });
-    });
+    }
 }
+
+smoothScroll();
+
+//Return to top button
+const hideHomeButton = () => {
+    const body = document.querySelector('body');
+    const homeButton = document.querySelector('.home');
+    window.addEventListener('scroll', (event) => {
+        if (window.scrollY >= body.offsetHeight / 2) {
+            homeButton.classList.remove('hide');
+        }
+        else {
+            homeButton.classList.add('hide');
+        }
+    })
+}
+hideHomeButton();
+
+const end = performance.now();
+console.log(end - start);
+console.log(document.body.offsetHeight)
